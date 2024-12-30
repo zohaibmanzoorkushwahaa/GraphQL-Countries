67dfe2b11f563caf43a2331f38041faec564ddb0
Zohaib Manzoor <zohaib.manzoor@arhamsoft.com>
2024-12-30 15:44:00 +0500

    initial commit

---
+++ CountryGraphQL/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CountryGraphQL
+//
+//  Created by Muhammad Irfan Zafar on 27/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CountryGraphQL/CountryGraphQLApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryGraphQLApp.swift
+//  CountryGraphQL
+//
+//  Created by Muhammad Irfan Zafar on 27/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct CountryGraphQLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
